a3054ba672a5e5ed4a98b02d7397d5d4824c3105
T2 <studioTeaTwo@gmail.com>
2024-01-21 23:43:24 +0900

    chore(mvp): transplant the code of lnc-web

---
+++ browser/components/wallet/src/lib/lnc/index.ts
@@ -0,0 +1,16 @@
+// tslint:disable-next-line
+require("./wasm_exec")
+
+import LNC from "./lnc"
+
+// polyfill
+if (!WebAssembly.instantiateStreaming) {
+  WebAssembly.instantiateStreaming = async (resp, importObject) => {
+    const source = await (await resp).arrayBuffer()
+    return await WebAssembly.instantiate(source, importObject)
+  }
+}
+
+export type { LncConfig, CredentialStore } from "./types/lnc"
+export * from "@lightninglabs/lnc-core"
+export default LNC
